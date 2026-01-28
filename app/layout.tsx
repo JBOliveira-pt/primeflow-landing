bf82407186fc64cda5c6c7c53f1080c66543f918
Jeison Oliveira <jeisonbr.pt@gmail.com>
2026-01-28 17:20:21 +0000

    fix: corrigir import de home para Home

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar/NavBar";
+import Navbar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 
 const geistSans = Geist({
